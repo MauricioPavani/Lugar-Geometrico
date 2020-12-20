e8d4c273e051039ac65bf843c9cba9d7a4f6db98
Mauricio <mauricio_pavani@hotmail.com>
2020-12-20 15:51:04 -0300

    Função para criar o polígono pronta

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ def anguloEntreVetores(ponto1, ponto2):
 	produto_escalar = produtoEscalar(ponto1, ponto2)
 
 	cos = produto_escalar/(norma1*norma2)
+	if cos > 1:
+		cos = 1
+	if cos < -1:
+		cos = -1
 	anguloRadianos = math.acos(cos)
 	anguloGraus = 180*anguloRadianos/np.pi
 
@@ -73,7 +77,18 @@ def calculaMaiorAngulo(vetorPontos, ref):
 	angulo = 0
 	for i in range(0, len(angulos)):
 		if angulos[i] > angulo:
+			angulo = angulos[i]
 			indice = i
+
+	return indice
+
+def buscaMenorR(vetorPontos):
+	menorR = vetorPontos[0][0]
+	indice = 0
+	for i in range(0, len(vetorPontos)):
+		if vetorPontos[i][0] < menorR:
+			indice = i
+			menorR = vetorPontos[i][0]
 
 	return indice
 
@@ -81,11 +96,47 @@ def pnl(vetorPontos):
 	vertices = []
 	pontoAleatorio = buscaPontoRandom(vetorPontos)
 	vertices = vertices + [calculaMenorAngulo(deslocaEixo(vetorPontos, vetorPontos[pontoAleatorio]))]
+
+	novoVetorPontos = deslocaEixo(vetorPontos, vetorPontos[vertices[0]])
+	vertices = vertices + [calculaMaiorAngulo(novoVetorPontos, novoVetorPontos[pontoAleatorio])]
+
+	plt.scatter(vetorPontos[pontoAleatorio][0], vetorPontos[pontoAleatorio][1], marker="o", color="red")
+	plt.scatter(vetorPontos[vertices[0]][0], vetorPontos[vertices[0]][1], marker="o", color="black")
+	plt.scatter(vetorPontos[vertices[1]][0], vetorPontos[vertices[1]][1], marker="o", color="green")
+
+	i = 1
+	while vertices[i] != vertices[0] and i < 100:
+		print(i)
+		novoVetorPontos = deslocaEixo(vetorPontos, vetorPontos[vertices[i]])
+		vertices = vertices + [calculaMaiorAngulo(novoVetorPontos, novoVetorPontos[vertices[i-1]])]
+		i = i + 1
+
+
+	novosVertices = set()
+	for x in vertices:
+		novosVertices.add(x)
+
+	poligono = []
+	for x in novosVertices:
+		poligono = poligono + [vetorPontos[x]]
+
+	vertices = []
+	pontoAleatorio = buscaPontoRandom(poligono)
+	vertices = vertices + [calculaMenorAngulo(deslocaEixo(poligono, poligono[pontoAleatorio]))]
+
+	novoVetorPontos = deslocaEixo(poligono, poligono[vertices[0]])
+	vertices = vertices + [calculaMaiorAngulo(novoVetorPontos, novoVetorPontos[pontoAleatorio])]
+
+	i = 1
+	while vertices[i] != vertices[0] and i < 100:
+		print(i)
+		novoVetorPontos = deslocaEixo(poligono, poligono[vertices[i]])
+		vertices = vertices + [calculaMaiorAngulo(novoVetorPontos, novoVetorPontos[vertices[i-1]])]
+		i = i + 1
+
+	return vertices
+
 	
-
-	# plt.scatter(vetorPontos[pontoAleatorio][0], vetorPontos[pontoAleatorio][1], marker="o", color="red")
-	# plt.scatter(vetorPontos[vertices[0]][0], vetorPontos[vertices[0]][1], marker="o", color="black")
-
 
 
 def nuvemPontos(pontosX, pontosY, color='blue'):
@@ -96,10 +147,11 @@ def nuvemPontos(pontosX, pontosY, color='blue'):
 def plotPnL(pontosX, pontosY, titulo=''):
 	nuvemPontos(pontosX, pontosY)
 	
-	plt.title('titulo')
+	plt.title(titulo)
 	plt.xlabel('G(S)')
 	plt.ylabel('B(S)')
-	pnl(formata(pontosX, pontosY))
+	vertices = pnl(formata(pontosX, pontosY))
+
 	plt.show()
 	# plt.savefig('test.png')
 
@@ -113,23 +165,28 @@ def formata(pontosX, pontosY):
 
 	return vetor
 
+def main(TextoX, TextoY):
+	arquivo = open(TextoX, 'r')
+	x = arquivo.read()
+	arquivo.close()
+	x = x.split('\n')
+	if x[-1] == '':
+		x.pop()
 
-arquivo = open('/home/mauricio/Documentos/MEGA/Projeto Lugar Geométrico/0/x.txt', 'r')
-x = arquivo.read()
-arquivo.close()
-x = x.split('\n')
-if x[-1] == '':
-	x.pop()
+	arquivo = open(TextoY, 'r')
+	y = arquivo.read()
+	arquivo.close()
+	y = y.split('\n')
+	if y[-1] == '':
+		y.pop()
 
-arquivo = open('/home/mauricio/Documentos/MEGA/Projeto Lugar Geométrico/0/y.txt', 'r')
-y = arquivo.read()
-arquivo.close()
-y = y.split('\n')
-if y[-1] == '':
-	y.pop()
+	n_x = [formatText(num) for num in x]
+	n_y = [formatText(num) for num in y]
 
-n_x = [formatText(num) for num in x]
-n_y = [formatText(num) for num in y]
+	plotPnL(n_x, n_y)
 
-plotPnL(n_x, n_y)
 
+
+TextoX = '/home/mauricio/Documentos/MEGA/Projeto Lugar Geométrico/0/x.txt'
+TextoY = '/home/mauricio/Documentos/MEGA/Projeto Lugar Geométrico/0/y.txt'
+main(TextoX, TextoY)
